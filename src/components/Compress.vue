83ae3a7496861b735592081e33028a7b8b143712
Azur Ramas <azur@bicomsystems.com>
2020-04-09 11:29:01 +0200

    Added Compression and exporting images (hard coded), need to add end dialog and dynamic compression quality

--- src/components/Compress.vue
+++ src/components/Compress.vue
@@ -4,7 +4,12 @@
     <v-container>
       <v-row>
         <v-col class="col-11">
-          <v-file-input webkitdirectory label="Select your output directory"></v-file-input>
+          <v-file-input
+            @change="getFolder"
+            @click="onInputClick"
+            webkitdirectory
+            label="Select your output directory"
+          ></v-file-input>
         </v-col>
         <v-col class="col-11">
           <v-subheader>Compression ratio</v-subheader>
@@ -16,21 +21,64 @@
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn color="error" text @click="closeDialog">Close</v-btn>
-      <v-btn color="success" text>Compress!</v-btn>
+      <v-btn color="success" @click="compress" text>Compress!</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import Compressor from "compressorjs";
 export default {
   data() {
     return {
-      ticksLabels: ["low", "medium", "optimal", "high", "extreme"]
+      ticksLabels: ["low", "medium", "optimal", "high", "extreme"],
+      exportPath: ""
     };
   },
   methods: {
     closeDialog() {
       this.$emit("closeDialog");
+    },
+    onInputClick: function(e) {
+      e.target.value = "";
+    },
+    getFolder(e) {
+      var folder = e.path;
+      console.log(folder);
+      this.exportPath = folder;
+    },
+    compress() {
+      var fs = require("fs");
+      var exportPath = this.exportPath;
+      
+      this.images.forEach(image => {
+        new Compressor(image, {
+          quality: 0.5,
+          maxWidth: 1920,
+          success(result) {
+            var reader = new FileReader();
+            reader.readAsArrayBuffer(result);
+            reader.onload = function() {
+              var buffer = new Buffer(reader.result);
+              fs.writeFile(exportPath + "\\" + image.name, buffer, {}, err => {
+                if (err) {
+                  console.error(err);
+                  return;
+                }
+                console.log("image saved");
+              });
+            };
+          },
+          error(err) {
+            console.log(err.message);
+          }
+        });
+      });
+    }
+  },
+  computed: {
+    images() {
+      return this.$store.getters.getImages;
     }
   }
 };
